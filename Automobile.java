419d21682075119296ffe4a2bbb6382310256196
Favor Vincent <vfavorm77@gmail.com>
2026-04-13 11:39:01 +0300

    Automobile Interface created

---
+++ Automobile.java
@@ -0,0 +1,14 @@
+public interface Automobile {
+    int fuelCapacity = 50;
+    int maxSpeed = 200; 
+
+    void accelerate(int speed);
+    void stop();
+    void checkEngine();
+    void checkTires();
+    void checkFuelLevel(int fuelLevel);
+
+
+
+    
+}
