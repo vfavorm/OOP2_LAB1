07f62f8443487feb2371313a0e350537d12a5679
Favor Vincent <vfavorm77@gmail.com>
2026-04-13 11:29:47 +0300

    Abstract class Vehicle created

---
+++ Vehicle.java
@@ -0,0 +1,20 @@
+public class Vehicle {
+    public String ownerName;
+    public String model;
+    public int speed;
+    public int fuelLevel;
+
+    
+    public void accelerate() {
+        
+    } 
+
+    public void stop() {
+
+    }
+    
+    public void gas() {
+
+    }
+ 
+}
